ed1fbb4bcdb56097789f09b521dedb92b2527eca
zzw <zzw.me@qq.com>
2019-12-30 22:33:03 +0800

    创建service层

---
+++ src/main/java/life/echo/community/dto/QuestionDTO.java
@@ -0,0 +1,7 @@
+package life.echo.community.dto;
+
+/**
+ * Created by zhangzewen on 2019/12/30
+ */
+public class QuestionDTO {
+}
---
+++ src/main/java/life/echo/community/service/QuestionService.java
@@ -0,0 +1,7 @@
+package life.echo.community.service;
+
+/**
+ * Created by zhangzewen on 2019/12/30
+ */
+public class QuestionService {
+}
