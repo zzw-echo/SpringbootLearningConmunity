dc883220218e00142a8c1437d89135e3773c9262
zzw <zzw.me@qq.com>
2020-01-08 12:12:08 +0800

    个人页面

--- src/main/java/life/echo/community/controller/ProfileController.java
+++ src/main/java/life/echo/community/controller/ProfileController.java
@@ -1,7 +1,9 @@
 package life.echo.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by zhangzewen on 2020/1/8
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProfileController {
 
-    @GetMapping("/profile")
-    public String profile(){
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable(name = "action") String action,
+                          Model model){
+
+        if ("questions".equals(action)){
+            model.addAttribute("section", "questions");
+        }
         return "profile";
     }
 }
