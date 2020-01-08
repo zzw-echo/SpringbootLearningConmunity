14a6484fc4a8fa1e3ed3ea55f282ea2d698d6b0a
zzw <zzw.me@qq.com>
2020-01-08 22:27:38 +0800

    个人页面

--- src/main/java/life/echo/community/controller/IndexController.java
+++ src/main/java/life/echo/community/controller/IndexController.java
@@ -1,8 +1,6 @@
 package life.echo.community.controller;
 
 import life.echo.community.dto.PaginationDTO;
-import life.echo.community.mapper.UserMapper;
-import life.echo.community.model.User;
 import life.echo.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -25,8 +22,6 @@ public class IndexController {
         return "index";
     }
 
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
@@ -37,19 +32,6 @@ public class IndexController {
                         Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "2") Integer size){
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length !=0){
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")){
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
         PaginationDTO pagination = questionService.list(page, size);
         model.addAttribute("pagination", pagination);
         return "index";
