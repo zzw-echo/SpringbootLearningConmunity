f3c521b360a5705d439d9122e6fea335553a9610
zzw <zzw.me@qq.com>
2020-01-19 19:21:47 +0800

    修复登录

--- src/main/java/life/echo/community/controller/AuthorizeController.java
+++ src/main/java/life/echo/community/controller/AuthorizeController.java
@@ -53,8 +53,7 @@ public class AuthorizeController {
             //name和id是从github获取的
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
-            user.setGmtCreate(System.currentTimeMillis());
-            user.setGmtModified(user.getGmtCreate());
+
             user.setAvatarUrl(githubUser.getAvatar_url());
             //用数据库实物的存储，代替了session的写入
 //            userMapper.insert(user);
