f2bbb7f8f92205a4cba05596a02ab35f587cc3ed
zzw <zzw.me@qq.com>
2020-01-30 23:45:46 +0800

    错误页面处理

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 https://www.bilibili.com/video/av65117012  
 
 ## 脚本
-mvn -Dmybatis.generator.overwrite=true mybatis-generator:generate
+mvn -Dmybatis.generator.overwrite=true mybatis-generator:generate  
 
 ## 资源网站
-http://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/
+http://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/  
+https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-caching-provider-couchbase  
   
 ------
 新年快乐！  
--- src/main/java/life/echo/community/advice/CustomizeExceptionHandler.java
+++ src/main/java/life/echo/community/advice/CustomizeExceptionHandler.java
@@ -1,7 +1,28 @@
 package life.echo.community.advice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by zhangzewen on 2020/1/30
  */
+@ControllerAdvice
 public class CustomizeExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    ModelAndView handle(HttpServletRequest request, Throwable ex) {
+        HttpStatus status = getStatus(request);
+        return new ModelAndView("error");
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.valueOf(statusCode);
+    }
 }
