4df12136ec11a5cfd5c7395350dd01f4a3fe892f
zzw <zzw.me@qq.com>
2020-01-31 23:18:39 +0800

    实现回复功能

--- src/main/java/life/echo/community/controller/CommentController.java
+++ src/main/java/life/echo/community/controller/CommentController.java
@@ -1,10 +1,17 @@
 package life.echo.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by zhangzewen on 2020/1/31
  */
 @Controller
 public class CommentController {
+
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
+    public Object post() {
+
+    }
 }
