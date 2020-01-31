0f10117502219bca3330756bc872ad476f418738
zzw <zzw.me@qq.com>
2020-01-31 23:33:33 +0800

    实现回复功能

--- src/main/java/life/echo/community/controller/CommentController.java
+++ src/main/java/life/echo/community/controller/CommentController.java
@@ -1,6 +1,11 @@
 package life.echo.community.controller;
 
+import life.echo.community.dto.CommentDTO;
+import life.echo.community.mapper.CommentMapper;
+import life.echo.community.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,8 +15,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CommentController {
 
-    @RequestMapping(value = "/comment", method = RequestMethod.POST)
-    public Object post() {
+    @Autowired
+    private CommentMapper commentMapper;
 
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO) {
+        Comment comment = new Comment();
+        commentMapper.insert(comment);
+        return null;
     }
 }
