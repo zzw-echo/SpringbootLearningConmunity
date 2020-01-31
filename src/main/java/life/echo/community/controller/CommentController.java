a307ec67bdf8cba0d942905982867471e80205d1
zzw <zzw.me@qq.com>
2020-01-31 23:16:13 +0800

    实现回复功能

---
+++ src/main/java/life/echo/community/controller/CommentController.java
@@ -0,0 +1,10 @@
+package life.echo.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zhangzewen on 2020/1/31
+ */
+@Controller
+public class CommentController {
+}
