5e9bee480965b901029037e3c50c8b4fbf692168
zzw <zzw.me@qq.com>
2020-02-01 23:21:04 +0800

    实现回复功能

--- src/main/java/life/echo/community/service/CommentService.java
+++ src/main/java/life/echo/community/service/CommentService.java
@@ -49,6 +49,7 @@ public class CommentService {
                 throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
             }
             commentMapper.insert(comment);
+            question.setCommentCount(1);
             questionExtMapper.incCommentCount(question);
         }
     }
