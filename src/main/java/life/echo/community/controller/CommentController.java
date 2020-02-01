f5cff9e9e4199da8e388c5b79c1c5aba32deb61a
zzw <zzw.me@qq.com>
2020-02-01 12:48:54 +0800

    实现回复功能

--- src/main/java/life/echo/community/controller/CommentController.java
+++ src/main/java/life/echo/community/controller/CommentController.java
@@ -1,8 +1,10 @@
 package life.echo.community.controller;
 
 import life.echo.community.dto.CommentDTO;
+import life.echo.community.dto.ResultDTO;
 import life.echo.community.mapper.CommentMapper;
 import life.echo.community.model.Comment;
+import life.echo.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,9 +28,14 @@ public class CommentController {
 
     @ResponseBody
     @RequestMapping(value = "/comment", method = RequestMethod.POST)
-    public Object post(@RequestBody CommentDTO commentDTO) {
-        Comment comment = new Comment();
+    public Object post(@RequestBody CommentDTO commentDTO,
+                       HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            return ResultDTO.errorOf(2002, "未登录不能进行评论，请先登录.. ");
+        }
 
+        Comment comment = new Comment();
         comment.setParentId(commentDTO.getParentId());
         comment.setContent(commentDTO.getContent());
         comment.setType(commentDTO.getType());
