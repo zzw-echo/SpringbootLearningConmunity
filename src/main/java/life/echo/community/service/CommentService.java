10616f2ea1276bd58d53bad20b402cde95050c4b
zzw <zzw.me@qq.com>
2020-02-01 18:15:07 +0800

    实现回复功能

--- src/main/java/life/echo/community/service/CommentService.java
+++ src/main/java/life/echo/community/service/CommentService.java
@@ -3,7 +3,10 @@ package life.echo.community.service;
 import life.echo.community.enums.CommentTypeEnum;
 import life.echo.community.exception.CustomizeErrorCode;
 import life.echo.community.exception.CustomizeException;
+import life.echo.community.mapper.CommentMapper;
+import life.echo.community.mapper.QuestionMapper;
 import life.echo.community.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,6 +14,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CommentService {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_NOT_FOUND);
@@ -18,6 +28,12 @@ public class CommentService {
 
         if (comment.getType() == null || !CommentTypeEnum.isExist(comment.getType())) {
             throw new CustomizeException(CustomizeErrorCode.TYPE_PARAM_WRONG);
+        }
+
+        if (comment.getType() == CommentTypeEnum.COMMENT.getType()){
+            //回复评论
+        } else {
+            //回复问题
         }
     }
 }
