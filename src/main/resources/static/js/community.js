299debf6ac0b29f8b6a3a1c16f79f4d21df3b78e
zzw <zzw.me@qq.com>
2020-02-03 18:39:25 +0800

    实现回复列表

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -17,7 +17,7 @@ function post() {
                 if (response.code == 2003) {
                     var isAccepted = confirm(response.message);
                     if (isAccepted) {
-                        window.open("https://github.com/login/oauth/authorize?client_id=b50c33320ed12a540e02&redirect_uri=http://localhost:8887/callback&scope=user&state=1","_self");
+                        window.open("https://github.com/login/oauth/authorize?client_id=b50c33320ed12a540e02&redirect_uri=http://localhost:8887/callback&scope=user&state=1","_blank");
                         window.localStorage.setItem("closable", true);
                     }
                 } else {
