2cb1f464e06588f918d2085a31714a20411eaf47
zzw <zzw.me@qq.com>
2020-02-05 18:10:25 +0800

    实现二级评论

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,3 +1,6 @@
+/**
+ * 提交回复
+ */
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
@@ -22,7 +25,7 @@ function post() {
                 if (response.code == 2003) {
                     var isAccepted = confirm(response.message);
                     if (isAccepted) {
-                        window.open("https://github.com/login/oauth/authorize?client_id=b50c33320ed12a540e02&redirect_uri=http://localhost:8887/callback&scope=user&state=1","_blank");
+                        window.open("https://github.com/login/oauth/authorize?client_id=b50c33320ed12a540e02&redirect_uri=http://localhost:8887/callback&scope=user&state=1", "_blank");
                         window.localStorage.setItem("closable", true);
                     }
                 } else {
@@ -34,3 +37,29 @@ function post() {
         dataType: "json"
     });
 }
+
+/**
+ * 展开二级回复
+ */
+function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-" + id);
+    // comments.toggleClass("in");
+
+    //获取二级评论展开状态
+    var collapse = e.getAttribute("data-collapse");
+    if (collapse) {
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    } else {
+        //展开二级评论
+        comments.addClass("in");
+        //标记二级评论展开状态
+        e.setAttribute("data-collapse", "in");
+        e.classList.add("active");
+    }
+}
+
+
