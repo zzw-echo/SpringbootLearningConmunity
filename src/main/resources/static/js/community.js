1406f5ae6278ea285b67a4194140652e38d19bb1
zzw <zzw.me@qq.com>
2020-02-06 18:19:26 +0800

    实现二级评论

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -4,6 +4,10 @@
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
+    comment2target(questionId, 1, content);
+}
+
+function comment2target(targetId, type, content) {
     if (!content) {
         alert("回复内容不能为空 ..");
         return;
@@ -13,9 +17,9 @@ function post() {
         url: "/comment",
         contentType: "application/json",
         data: JSON.stringify({
-            "parentId": questionId,
+            "parentId": targetId,
             "content": content,
-            "type": 1
+            "type": type
         }),
         success: function (response) {
             if (response.code == 200) {
@@ -31,11 +35,16 @@ function post() {
                 } else {
                     alert(response.message);
                 }
-
             }
         },
         dataType: "json"
     });
+}
+
+function comment(e) {
+    var commentId = e.getAttribute("data-id");
+    var content = $("#input-" + commentId).val();
+    comment2target(commentId, 2, content);
 }
 
 /**
@@ -54,11 +63,29 @@ function collapseComments(e) {
         e.removeAttribute("data-collapse");
         e.classList.remove("active");
     } else {
-        //展开二级评论
-        comments.addClass("in");
-        //标记二级评论展开状态
-        e.setAttribute("data-collapse", "in");
-        e.classList.add("active");
+        $.getJSON("/comment/" + id, function (data) {
+            var commentBody = $("comment-body-" + id);
+            var items = [];
+            $.each(data.data, function (comment) {
+                var c = $("<div/>", {
+                    "class": "col-lg-12 col-md-12 col-sm-12 col-xs-12 comments",
+                    html: comment.content
+                });
+                items.push(c);
+            });
+
+            $("<div/>", {
+                "class": "col-lg-12 col-md-12 col-sm-12 col-xs-12 collapse sub-comments",
+                "id": "comment-" + id,
+                html: items.join("")
+            }).appendTo(commentBody);
+            //展开二级评论
+            comments.addClass("in");
+            //标记二级评论展开状态
+            e.setAttribute("data-collapse", "in");
+            e.classList.add("active");
+        });
+
     }
 }
 
