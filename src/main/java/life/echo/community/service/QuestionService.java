1cfd432b1119eb1bac9770ff1f2ef6e6271ddc40
zzw <zzw.me@qq.com>
2020-02-26 21:29:47 +0800

    切换mysql

--- src/main/java/life/echo/community/service/QuestionService.java
+++ src/main/java/life/echo/community/service/QuestionService.java
@@ -100,7 +100,7 @@ public class QuestionService {
         paginationDTO.setPagination(totalPage, page);
 
         //size*(page-1)
-        Integer offset = size * (page - 1);
+        Integer offset = page < 1 ? 0 : size * (page - 1);
         QuestionExample example = new QuestionExample();
         example.createCriteria()
                 .andCreatorEqualTo(userId);
